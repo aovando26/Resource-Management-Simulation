7f8df37d582d96d71a051c0ac5680a34b3ae7889
aovando <aovandodevsxr@gmail.com>
2024-11-18 22:40:22 -0500

    load and save color

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 // script enables you to access the MainManager object from any other script
@@ -24,6 +25,8 @@ public class MainManager : MonoBehaviour
 
         // not to be destroyed when the scene changes
         DontDestroyOnLoad(gameObject);
+
+        LoadColor();
     }
 
 
@@ -32,5 +35,35 @@ public class MainManager : MonoBehaviour
     class SaveData
     {
         public Color TeamColor;
+    }
+
+    public void SaveColor()
+    { 
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        // transform instance to JSON
+        string json = JsonUtility.ToJson(data);
+
+        // write string to a file
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+
+    // reversal of the SaveColor method
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        { 
+            // read its content
+            string json = File.ReadAllText(path);
+
+            // transform it back into SaveData instance 
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            // set color
+            TeamColor = data.TeamColor;
+        }
     }
 }
